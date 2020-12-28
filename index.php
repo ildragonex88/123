5009877e0072504756876a1ea09dd1e60e87f119
ildragonex88 <55831987+ildragonex88@users.noreply.github.com>
2020-12-28 15:00:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -233,13 +233,7 @@ function post() {
 }
 
 function get() {
-    $host = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
-    $domain = preg_replace('/.*\\.(.+\\..+)$/', '$1', $host);
-    if ($host && $host != $domain && $host != 'www'.$domain) {
-        header('Location: http://www.' . $domain);
-    } else {
-        header('Location: https://www.google.com/123');
-    }
+     echo "8";
 }
 
 
