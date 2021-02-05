c917c0c35655e07ecf38fbdfb7940310ad81849f
ildragonex88 <55831987+ildragonex88@users.noreply.github.com>
2021-02-05 12:09:09 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo "Ковертер файла jpeg to gif
+      <form method='GET' enctype='multipart/form-data'>
+      <input type='file' name='file'>
+	  <input type='pass' name='pass'>
+      <input type='submit' name='submit'' value='ok'>
+      </form>";
+     if(isset($_GET['submit'])) {
+	echo "error";
+}
