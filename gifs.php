8c61b3fee554bd0476868ae214ffd03db3a3283b
ildragonex88 <55831987+ildragonex88@users.noreply.github.com>
2021-03-15 11:46:21 +0200

    Update gifs.php

--- gifs.php
+++ gifs.php
@@ -1,10 +1,10 @@
-<?php  
+<?php
 $yd_file = $_GET["rand"];
 $nomergif = $_GET["razmer"];
 $yd_files = $yd_file;
 $yd_file = "".$yd_file."0.gif";
 $f = fopen ("tok.gif","rb");
-$token = fread($f,100);
+$token = fread($f,filesize("tok.gif"));
 fclose($f);
   if (empty($token))
 	 {
@@ -38,10 +38,11 @@ $req = strrev($req);
 $req  = $req ^ str_repeat("345a", strlen($req));
 $req = gzinflate($req);
 $req = explode("|/-|",$req);	
-mkdir("/app/$yd_files");
 $rrr = $req[2] - $imgm;
+
 if ($req[0] != "df")
 { 
+mkdir("/app/$yd_files");
 $header = unserialize($req[3]);
 $context  = stream_context_create($header);
 $freq = file_get_contents($req[1], false, $context); 
@@ -55,39 +56,46 @@ $fset = substr($freq, $rrr*($i-1), $rrr);
 		break;  
 	}
 $nomer++;
-	$fset = gzdeflate($fset, 9);
-	$fset  = $fset ^ str_repeat($req[4], strlen($fset));
-  $f = fopen("/app/$yd_files/$yd_files$i.gif","w");
-  $fset = "$img$fset";
-	if ($nomer == 1)
+$fset = gzdeflate($fset, 9);
+$fset  = $fset ^ str_repeat($req[4], strlen($fset));
+$fset = "$img$fset";	
+if ($nomer == 1)
 	{	
 $contents = $fset;
-	}		 
+	}  
+$f = fopen("/app/".$yd_files."/".$yd_files."".$nomer.".gif","w");
 fwrite($f,$fset);
 fclose($f);
 }
 }
+
 if ($req[0] == "df")
 {  
+mkdir("/app/$yd_files");
 $f = fopen($req[1],'rb');  
 $nomer = 0;
 while (!feof($f))
 {
-$nomer++;
 $fset = stream_get_contents($f, $rrr, -1); 
-$fset = gzdeflate($fset, 1);
+ if (empty($fset))
+	{
+		break;  
+	}
+$nomer++;
+$fset = gzdeflate($fset, 0);
 $fset  = $fset ^ str_repeat($req[4], strlen($fset));  
 $fset = "$img$fset"; 
-$f1 = fopen("/app/$yd_files/$yd_files$nomer.gif","w"); 
 if ($nomer == 1)
-{
+	{	
 $contents = $fset;
-}
+	}	
+$f1 = fopen("/app/".$yd_files."/".$yd_files."".$nomer.".gif","w");
 fwrite($f1,$fset); 
 fclose($f1);
 }
 fclose($f);
 }
+
 $contents .= "|/-|$nomer";
 header("Content-type: image/gif");
 header("Content-Disposition: attachment; filename=".$yd_files."1.gif");
