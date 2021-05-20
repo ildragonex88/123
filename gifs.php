e17488636832e6b43e83a79e0edff9deb6164e96
ildragonex88 <55831987+ildragonex88@users.noreply.github.com>
2021-05-20 12:24:07 +0300

    Update gifs.php

--- gifs.php
+++ gifs.php
@@ -1,19 +1,22 @@
-<?php
-$yd_file = $_GET["rand"];
+<?php 
+function rep($x)
+{ $x = base_convert($x, 10, 26);
+return $x; }
+$rand = $_GET["rand"];
 $nomergif = $_GET["razmer"];
-$yd_files = $yd_file;
-$yd_file = "".$yd_file."0.gif";
+$randdrep1 = "".$rand."1";
+$randdrep1 = rep($randdrep1);
+$randdrep0 = "".$rand."0";
+$randdrep0 = rep($randdrep0);
 $f = fopen ("tok.gif","rb");
 $token = fread($f,filesize("tok.gif"));
 fclose($f);
-  if (empty($token))
-	 {
+if (empty($token)){
 header("Content-type: image/gif");
-header("Content-Disposition: attachment; filename=".$yd_files."1.gif");
-echo 't';
-exit;
-	 } 
-$ch = curl_init('https://cloud-api.yandex.net/v1/disk/resources/download?path=' . urlencode($yd_file));
+header("Content-Disposition: attachment; filename=".$randdrep1.".gif");
+echo 'nt'; exit; } 
+$randdrepg0 = "".$randdrep0.".gif";
+$ch = curl_init('https://cloud-api.yandex.net/v1/disk/resources/download?path=' . urlencode($randdrepg0));
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: OAuth ' . $token));
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -24,79 +27,81 @@ $req = explode(':"', $req);
 $req = str_replace('https:','http:',$req[1]);
 $req = explode('","', $req);
 $req = file_get_contents($req[0]);
- if (empty($req))
-	 {
- header("Content-type: image/gif");
- header("Content-Disposition: attachment; filename=".$yd_files."1.gif");
- echo 'y';
-exit;
-	 }
+if (empty($req)){ 
+header("Content-type: image/gif");
+header("Content-Disposition: attachment; filename=".$randdrep1.".gif");
+echo 'ny'; exit; }
 $img  = substr($req, 0, $nomergif);
 $req  = substr($req, $nomergif);
 $imgm = strlen($img);
 $req = strrev($req);
 $req  = $req ^ str_repeat("345a", strlen($req));
 $req = gzinflate($req);
-$req = explode("|/-|",$req);	
+$req = explode("/-|",$req);	
 $rrr = $req[2] - $imgm;
-
-if ($req[0] != "df")
+$url = base64_decode($req[1]);
+$met = $req[0];
+$contenttype = $req[5];
+$contenttype = explode("%-|",$contenttype);
+$razr = $contenttype[1];
+mkdir("/app/$rand");
+if ($met == 'df') {
+$f = fopen($url,'rb');  
+$nomer = 0;
+$n = 1;
+while (!feof($f)) {
+$rrrstr = 0;
+$randdrepn = "$rand$n";
+$randdrepn = rep($randdrepn);
+$f1 = fopen("/app/".$rand."/".$randdrepn.".".$razr."","a"); 
+for($i=1;$i<=400;$i++) {	
+$fset = stream_get_contents($f, 131072, -1);
+if (empty($fset)) {
+break; }
+$rrrstr = $rrrstr+131072;
+$fset  = $fset ^ str_repeat($req[4], strlen($fset));  
+if  ($i == 1) {
+$fset = "$img$fset"; }
+fwrite($f1,$fset);
+if ($rrrstr >= $rrr) {
+break; }
+}
+fclose($f1);
+$nomer++;
+$n++;
+}
+fclose($f);
+$f3 = fopen ("/app/".$rand."/".$randdrep1.".".$razr."","rb"); 
+$contents = fread($f3,filesize("/app/".$rand."/".$randdrep1.".".$razr.""));
+fclose($f3);
+}
+else
 { 
-mkdir("/app/$yd_files");
 $header = unserialize($req[3]);
 $context  = stream_context_create($header);
-$freq = file_get_contents($req[1], false, $context); 
+$freq = file_get_contents($url, false, $context); 
 $header = serialize($http_response_header);
-$freq = "$header|/-|$freq";
+$freq = "$header/-|$freq";
 $nomer = 0;
 for($i=1;$i<=400;$i++){	
+$randdrepn = "$rand$i";
+$randdrepn = rep($randdrepn);
 $fset = substr($freq, $rrr*($i-1), $rrr); 
-	 if (empty($fset))
-	{
-		break;  
-	}
+if (empty($fset)) {
+break; }
 $nomer++;
 $fset = gzdeflate($fset, 9);
 $fset  = $fset ^ str_repeat($req[4], strlen($fset));
-$fset = "$img$fset";	
-if ($nomer == 1)
-	{	
-$contents = $fset;
-	}  
-$f = fopen("/app/".$yd_files."/".$yd_files."".$nomer.".gif","w");
+$fset = "$img$fset";
+$f = fopen("/app/".$rand."/".$randdrepn.".".$razr."","w");
 fwrite($f,$fset);
-fclose($f);
-}
-}
-
-if ($req[0] == "df")
-{  
-mkdir("/app/$yd_files");
-$f = fopen($req[1],'rb');  
-$nomer = 0;
-while (!feof($f))
-{
-$fset = stream_get_contents($f, $rrr, -1); 
- if (empty($fset))
-	{
-		break;  
-	}
-$nomer++;
-$fset = gzdeflate($fset, 0);
-$fset  = $fset ^ str_repeat($req[4], strlen($fset));  
-$fset = "$img$fset"; 
-if ($nomer == 1)
-	{	
-$contents = $fset;
-	}	
-$f1 = fopen("/app/".$yd_files."/".$yd_files."".$nomer.".gif","w");
-fwrite($f1,$fset); 
-fclose($f1);
-}
-fclose($f);
-}
-
-$contents .= "|/-|$nomer";
-header("Content-type: image/gif");
-header("Content-Disposition: attachment; filename=".$yd_files."1.gif");
-echo $contents;
+fclose($f); }
+$f1 = fopen ("/app/".$rand."/".$randdrep1.".".$razr."","rb");
+$contents = fread($f1,filesize("/app/".$rand."/".$randdrep1.".".$razr.""));
+fclose($f1); }
+$nomer = base64_encode($nomer);
+$nomer = gzdeflate($nomer, 9);
+$contents .= "/-|$nomer";
+header("Content-type: ".$contenttype[0]."");
+header("Content-Disposition: attachment; filename=".$randdrep1.".".$razr."");
+echo $contents;  
